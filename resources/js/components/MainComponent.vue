fe0e0737733c4c5bff232cc4936ea1a33c9556e6
IhzaMZ <sidoelz.123@gmail.com>
2024-02-11 20:51:18 +0700

    init project

---
+++ resources/js/components/MainComponent.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <h1>Semua Produk</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Stock</th>
+                        <th>Price</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(allProduct, index) in allProducts" :key="index" class="table align-middle">
+                        <td>{{ allProduct.name }}</td>
+                        <td>{{ allProduct.description }}</td>
+                        <td>{{ allProduct.stock }}</td>
+                        <td>Rp. {{ allProduct.price }}</td>
+                        <td>
+                            <button class="btn btn-primary" type="submit" @click="functionAddToCart(allProduct)"
+                                v-if="allProduct.stock > 0">
+                                Add to cart
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <ShoppingCartComponent :shoppingCartDatas="cartProducts" :totalPassing="total" @emit-click="functionDelete"
+                @emit-checkout="functionCheckout"></ShoppingCartComponent>
+        </div>
+    </div>
+</template>
+
+<script>
+import ShoppingCartComponent from './ShoppingCart.vue';
+
+export default {
+    data: function () {
+        return {
+            allProducts: [
+                {
+                    name: "Indomie Goreng Rendang",
+                    description: "Masakan instan terenak di dunia",
+                    stock: 10,
+                    price: 3900,
+                },
+                {
+                    name: "Mie Gelas Rendang",
+                    description: "Mie instant khusus anak kosan",
+                    stock: 5,
+                    price: 1500,
+                },
+                {
+                    name: "Bakmi Mewah",
+                    description: "Kalau anak kosan jangan macem2 deh",
+                    stock: 80,
+                    price: 10000,
+                },
+            ],
+            cartProducts: [],
+            total: 0,
+            dataPass: "Data Passing 222",
+            newStock: 0,
+        };
+    },
+    methods: {
+        functionAddToCart(allProducts) {
+            allProducts.stock--;
+            const existingIndex = this.cartProducts.findIndex(item => item.name === allProducts.name);
+            if (existingIndex >= 0) {
+                this.cartProducts[existingIndex].stock++;
+                this.cartProducts[existingIndex].price += allProducts.price;
+            }
+            else {
+                this.cartProducts.push({
+                    name: allProducts.name,
+                    description: allProducts.description,
+                    stock: 1,
+                    price: allProducts.price,
+                });
+            }
+            this.total += allProducts.price;
+        },
+        functionDelete(index) {
+            this.total = this.total - this.cartProducts[index].price;
+            this.allProducts.forEach((data) => {
+                if (data.name === this.cartProducts[index].name) {
+                    data.stock += this.cartProducts[index].stock
+                };
+            },);
+            this.cartProducts.splice(index, 1);
+            console.log(index);
+        },
+        functionCheckout() {
+            alert(`Pay us ${this.total}`)
+        },
+    },
+    mounted() {
+        console.log("Component mounted.");
+    },
+    components: { ShoppingCartComponent }
+};
+</script>
---
+++ resources/js/components/ShoppingCart.vue
@@ -0,0 +1,57 @@
+<template>
+    <h1>Keranjang Belanja</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Price</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(item, index) in shoppingCartDatas" :key="index" class="table align-middle">
+                <td>{{ item.name }}</td>
+                <td>{{ item.stock }}</td>
+                <td>Rp. {{ item.price }}</td>
+                <td>
+                    <button class="btn btn-danger text-bg-danger" @click="functionClick(index)">Delete</button>
+                </td>
+            </tr>
+            <tr>
+                <th colspan="2">Total:</th>
+                <th>Rp. {{ totalPassing }}</th>
+            </tr>
+        </tbody>
+    </table>
+    <button class="btn btn-success" @click="functionCheckout()">Checkout</button>
+
+    <!-- <button class="btn btn-primary" @click="$emit('emit-click', 'Params')">Submit Button</button> -->
+    <!-- <button class="btn btn-primary" @click="$emit('emit-click', listData)"></button> -->
+</template>
+
+<script>
+export default {
+    emits: ['emit-click', 'emit-checkout'],
+    props: {
+        totalPassing: Number,
+        titlePass: String,
+        shoppingCartDatas: {
+            type: Array,
+            default: () => {
+                return [];
+            },
+        },
+    },
+    methods: {
+        functionClick(item) {
+            this.$emit('emit-click', item)
+        },
+        functionCheckout() {
+            this.$emit('emit-checkout')
+        },
+    }
+}
+</script>
+
+<style></style>
